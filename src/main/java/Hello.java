5160677176a55a8f17572389e8cf033fe61f35e2
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 19:42:01 -0500

    master2 new changes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello people!");
         //new commit added
+        //master2 new commit added
 
     }
 }
